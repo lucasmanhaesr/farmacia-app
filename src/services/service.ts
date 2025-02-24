342c363c11c375c763f7261027b75001a7fc5759
Lucas Manhães <ramosmanhaes@outlook.com>
2025-02-24 10:26:31 -0300

    Add: React Popup e React Toastify

--- src/services/service.ts
+++ src/services/service.ts
@@ -4,8 +4,3 @@ const api = axios.create({
     baseURL: 'https://farmacia-jjxo.onrender.com'
 })
 
-export const consultar = async (url: string, setDados: Function) => {
-    const resposta = await api.get(url)
-    setDados(resposta.data)
-}
-
